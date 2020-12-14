1efef2a5d8a5c484f7d98e7f9e5deb40661468bd
merdy <sampekmoy@gmail.com>
2020-12-14 15:39:36 +0300

    add nginx config

--- src/handlers.py
+++ src/handlers.py
@@ -5,12 +5,6 @@ from aiohttp import web
 
 logger.add('logs/out.log')
 
-headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers': '*'
-}
-
 
 async def handle_pdf_request(request):
     """converter from html-layout to pdf-blob"""
@@ -27,10 +21,6 @@ async def handle_pdf_request(request):
         logger.info(f'{round(time() - start_time, 2)} sec. - {len(req["html"])} page(s)')
         logger.info('---- END OF REQUEST -----')
 
-        return web.Response(body=pdf_blob, headers=headers)
+        return web.Response(body=pdf_blob)
 
-    return web.Response(text='{success: false}', headers=headers)
-
-
-def handle_pdf_options_request(request):
-    return web.Response(headers=headers)
+    return web.Response(text='{success: false}')
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from aiohttp import web
-from src.handlers import handle_pdf_request, handle_pdf_options_request
+from src.handlers import handle_pdf_request
 
 
 app = web.Application()
-app.add_routes([web.post('/', handle_pdf_request), web.options('/', handle_pdf_options_request)])
+app.add_routes([web.post('/', handle_pdf_request)])
 
 if __name__ == '__main__':
     web.run_app(app, port=80)
